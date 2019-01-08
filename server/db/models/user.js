e983887012ebf35280df315cf56c02e39c6d9e3c
Kayla <keoliva@andrew.cmu.edu>
2019-01-08 15:41:30 -0500

    chore: basic user model
    updated api user route and test user route since User requires a name

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,10 +3,20 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
     allowNull: false
+  },
+  cart: {
+    type: Sequelize.ARRAY(Sequelize.INT)
   },
   password: {
     type: Sequelize.STRING,
