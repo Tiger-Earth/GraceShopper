626efe1f6b29de64c556e46a46c89d4d84c47907
McRae Petrey <mcrae@McRaes-MacBook-Pro-2018.local>
2019-01-08 15:44:05 -0500

    add orders model

---
+++ server/db/models/orders.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('order', {
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: new Date()
+  },
+  total: {
+    type: Sequelize.DECIMAL
+  },
+  items: {
+    type: Sequelize.ARRAY(Sequelize.INT)
+  }
+})
+
+module.exports = Order
