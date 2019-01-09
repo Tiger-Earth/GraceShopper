c2d5f934e4e00234dbdff86ddb3ccac154578047
Kayla <keoliva@andrew.cmu.edu>
2019-01-09 16:21:42 -0500

    - moved winesList component to its own folder - added a Route for a single view of a wine

--- client/components/index.js
+++ client/components/index.js
@@ -6,4 +6,5 @@
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
 export {Login, Signup} from './auth-form'
-export {default as WinesList} from './winesList'
+export {default as SingleWine} from './SingleWine/SingleWineContainer'
+export {default as WinesList} from './WineList/winesList'
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, WinesList} from './components'
+import {Login, Signup, UserHome, WinesList, SingleWine} from './components'
 import {me} from './store'
 
 /**
@@ -19,6 +19,7 @@ class Routes extends Component {
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route exact path="/" component={WinesList} />
+        <Route exact path="/wines/:wineId" component={SingleWine} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
