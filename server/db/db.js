414c95f39fb86826d8b458cf6f5723fd8dc9860e
saecula <krgilb@gmail.com>
2019-01-11 10:51:10 -0500

    db url bug temp fix

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize')
 require('../../secrets')
 
+const pkg = require('../../package.json')
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+process.env.DATABASE_URL = `postgres://localhost:5432/${databaseName}`
+
 const db = new Sequelize(process.env.DATABASE_URL, {
   logging: false
 })
