ea873c58bde2a8098be55b4aee44d6a37dd00bf9
saecula <krgilb@gmail.com>
2019-01-11 06:28:58 -0500

    moved session_s var

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ const createApp = () => {
   // session middleware with passport
   app.use(
     session({
-      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      secret: process.env.SESSION_SECRET,
       store: sessionStore,
       resave: false,
       saveUninitialized: false
