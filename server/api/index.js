203656603bae87408adef5a4f68bbe615827d586
saecula <krgilb@gmail.com>
2019-01-11 12:05:45 -0500

    fixy fix

--- server/api/index.js
+++ server/api/index.js
@@ -1,17 +1,10 @@
 const router = require('express').Router()
-module.exports = {router, isAuthenticated}
+module.exports = router
 
+router.use('/users', require('./users'))
 router.use('/wines', require('./wines'))
 router.use('/orders', require('./orders'))
 router.use('/cart', require('./cart'))
-
-function isAuthenticated(req, res, next) {
-  if (process.env.NODE_ENV === 'test' || (req.user && req.session.user.id)) {
-    return next()
-  } else {
-    res.redirect('/')
-  }
-}
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
