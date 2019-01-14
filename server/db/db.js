6c5d98f076db0be110fefb7892296c8213a1cae7
saecula <krgilb@gmail.com>
2019-01-14 14:16:21 -0500

    travis fix fix

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 require('../../secrets')
 
-const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize(process.env.DATABASE_URL || 'www.fake.com', {
   logging: false
 })
 module.exports = db
