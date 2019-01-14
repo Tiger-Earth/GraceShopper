57eece0b35bb7fa531bcaa689363ae11f121e9b9
saecula <krgilb@gmail.com>
2019-01-14 14:20:23 -0500

    again

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize')
 require('../../secrets')
 
-const db = new Sequelize(process.env.DATABASE_URL || 'www.fake.com', {
-  logging: false
-})
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/GraceShopper',
+  {
+    logging: false
+  }
+)
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
