462941fe807929dedcf3eb7cd6ee7e7d546f4ce6
McRae Petrey <mcrae@McRaes-MacBook-Pro-2018.local>
2019-01-13 20:32:08 -0500

    bug: circle warps with 1 digit

--- client/components/CartIcon.js
+++ client/components/CartIcon.js
@@ -2,17 +2,19 @@ import React from 'react'
 import {FiShoppingBag} from 'react-icons/fi'
 
 const CartIcon = props => {
+  let total
   const cart = props.cart
   const totalArray = Object.values(cart)
-  const total = totalArray.reduce((acc, el) => acc + el)
+  if (totalArray.length === 0) {
+    total = undefined
+  } else {
+    total = totalArray.reduce((acc, el) => acc + el)
+  }
+
   return (
-    // <div className="cart-notification">
-    //   <FiShoppingBag id="shopping-cart" />
-    //   <div className="badge">{total}</div>
-    // </div>
     <a href="#" className="notification">
       <FiShoppingBag />
-      <span className="badge">{total}</span>
+      {total ? <span className="badge">{total}</span> : ''}
     </a>
   )
 }
--- client/components/CartIcon.spec.js
+++ client/components/CartIcon.spec.js
@@ -9,14 +9,22 @@ import CartIcon from './CartIcon'
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-describe('CartIcon', () => {
-  let wrapper
+describe('CartIcon with items', () => {
+  const props = {cart: {1: 21}}
 
-  beforeEach(() => {
-    wrapper = shallow(<CartIcon cart="21" />)
-  })
+  const renderedMiniCart = shallow(<CartIcon props={props} />)
 
   it('renders the badge with cart quantity', () => {
-    expect(wrapper.find('span').text()).to.be.equal('21')
+    expect(renderedMiniCart.find('span').text()).to.be.equal('21')
   })
 })
+
+// describe('CartIcon with empty cart', () => {
+//   const cart = {}
+
+//   const renderedCart = shallow(<CartIcon cart={cart} />)
+
+//   it('does not render if there is no quantity', () => {
+//     expect(renderedCart.contains(<span />)).to.be.equal(false)
+//   })
+// })
