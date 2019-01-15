84bde1f38440782bf920cdf12c94c2522625a9e5
Kayla <keoliva@andrew.cmu.edu>
2019-01-15 18:47:37 -0500

    aligns with columns

--- client/components/Cart/CartRow.js
+++ client/components/Cart/CartRow.js
@@ -6,41 +6,39 @@ const CartRow = props => {
   const {wine, handleChange} = props
 
   return (
-    <div>
-      {wine.quantity && (
-        <TableRow key={wine.id}>
-          <TableCell>{wine.name}</TableCell>
-          <TableCell>
-            <img src={wine.imageURL} style={{width: 150, border: 1}} />
-          </TableCell>
-          <td>
-            <select
-              type="number"
-              id={wine.id}
-              defaultValue={wine.quantity}
-              onChange={handleChange}
-            >
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-            </select>
-          </td>
-          <TableCell align="right">
-            {Math.floor(wine.price / 100)}.{wine.price % 100}
-          </TableCell>
-        </TableRow>
-      )}
-    </div>
+    wine.quantity && (
+      <TableRow key={wine.id}>
+        <TableCell>
+          <img src={wine.imageURL} style={{width: 150, border: 1}} />
+          {wine.name}
+        </TableCell>
+        <TableCell align="right">
+          <select
+            type="number"
+            id={wine.id}
+            defaultValue={wine.quantity}
+            onChange={handleChange}
+          >
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+        </TableCell>
+        <TableCell align="right">
+          {Math.floor(wine.price / 100)}.{wine.price % 100}
+        </TableCell>
+      </TableRow>
+    )
   )
 }
 
