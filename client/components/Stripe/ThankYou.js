20cdc610e31444de424a449c8b31fe9fd4f3cfe2
McRae Petrey <mcrae@McRaes-MacBook-Pro-2018.local>
2019-01-15 17:26:58 -0500

    style: ty page

--- client/components/Stripe/ThankYou.js
+++ client/components/Stripe/ThankYou.js
@@ -9,11 +9,9 @@ export class ThankYou extends Component {
   render() {
     return (
       <div id="thank-you">
-        {/* <p>Thank you for your order!</p> */}
-        <CardMedia
-          style={{height: 0, paddingTop: '56.25%', borderRadius: 5}}
-          image="https://assets.bonappetit.com/photos/57ae42641b33404414975c3f/16:9/w_2560,c_limit/wildair-new-york-restaurant-bar-main-use-this-.jpg"
-        />
+        <div id="thank-you-div">
+          <p>Thank you for your order!</p>
+        </div>
       </div>
     )
   }
