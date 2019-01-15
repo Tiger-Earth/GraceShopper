5d06b1054a5b5d236c27e2a5959914e3f16244ea
McRae Petrey <mcrae@McRaes-MacBook-Pro-2018.local>
2019-01-15 13:31:42 -0500

    thank you page tweaks

--- client/components/Stripe/ThankYou.js
+++ client/components/Stripe/ThankYou.js
@@ -6,7 +6,7 @@ export const ThankYou = () => {
     <div id="thank-you">
       <p>Thank you for your order!</p>
       <CardMedia
-        style={{height: 0, borderRadius: 5}}
+        style={{height: 0, paddingTop: '56.25%', borderRadius: 5}}
         image="https://assets.bonappetit.com/photos/57ae42641b33404414975c3f/16:9/w_2560,c_limit/wildair-new-york-restaurant-bar-main-use-this-.jpg"
       />
     </div>
